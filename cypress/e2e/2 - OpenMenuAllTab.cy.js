1c8d8800e81abfd7a96a03fdc0ff0256279045c9
Muhammad Ekananda Alfandza Fajar <muhammadfajar16@hotmail.com>
2024-01-17 21:43:38 +0700

    change loop

--- cypress/e2e/2 - OpenMenuAllTab.cy.js
+++ cypress/e2e/2 - OpenMenuAllTab.cy.js
@@ -4,7 +4,7 @@ describe('Open Menu All Tab', () => {
     cy.get('[src="https://www.asia-quest.jp/hs-fs/hubfs/raw_assets/public/AsiaQuest_August2021/images/logo-white.png?width=1000&name=logo-white.png"]').should('exist')
     cy.get('#hs-eu-confirmation-button').click()
 
-    for (let i = 0; i < 6; i++){
+    for (let i = 0; i < 7; i++){
       cy.get('[class="hs-menu-item hs-menu-depth-1 hs-item-has-children"]').eq(i).should('exist').click().wait(2000)
       cy.get('[class="hs-menu-item hs-menu-depth-1 hs-item-has-children is-open"]').should('exist').click().wait(2000)
     }
